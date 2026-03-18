3b4ec281a99f089280a4ee0e715933b596187494
erialsan <151856104+erialsan@users.noreply.github.com>
2026-03-18 23:54:23 +0900

    Add files via upload

---
+++ src/main/java/alice/af/client/entity/EntityFillerFrame.java
@@ -0,0 +1,95 @@
+package alice.af.client.entity;
+
+import java.lang.ref.WeakReference;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+import org.apache.logging.log4j.Logger;
+
+import alice.af.AdvancedFiller;
+import alice.af.tileentity.TileEntityAdvFiller;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public final class EntityFillerFrame extends Entity {
+
+    public final Logger LOG;
+    public final WeakReference<TileEntityAdvFiller> filler;
+
+    public EntityFillerFrame(TileEntityAdvFiller filler) {
+        super(filler.getWorldObj());
+        this.filler = new WeakReference<TileEntityAdvFiller>(filler);
+
+        this.setSize(1F, 1F);
+        // レンダリングにはlastTickPosが使われるのでこのメソッドを使用
+        this.setLocationAndAngles(filler.xCoord + 0.5, filler.yCoord, filler.zCoord + 0.5, 0, 0);
+
+        this.prevPosX = filler.xCoord;
+        this.prevPosY = filler.yCoord;
+        this.prevPosZ = filler.zCoord;
+
+        LOG = AdvancedFiller.LOG;
+    }
+
+    @Override
+    protected void entityInit() {
+        this.ignoreFrustumCheck = true;
+    }
+
+    @Override
+    protected boolean canTriggerWalking() {
+        return false;
+    }
+
+    @Override
+    public boolean canBeCollidedWith() {
+        return !this.isDead;
+    }
+
+    @Override
+    public void onEntityUpdate() {
+        super.onEntityUpdate();
+
+        World world = this.worldObj;
+
+        if (!world.isRemote) {
+            throw new IllegalStateException("SERVER SIDE!");
+        }
+
+        TileEntityAdvFiller f = this.filler.get();
+        if (f == null) {
+            LOG.info("TileEntity at {},{},{} has gone.", this.posX, this.posY, this.posZ);
+            this.setDead();
+            return;
+        }
+
+        TileEntity te = world.getTileEntity(f.xCoord, f.yCoord, f.zCoord);
+        if ((te == null) || (te != f)) {
+            LOG.info("Instance cur={} org={}", (te != null) ? te.hashCode() : 0, f.hashCode());
+            f.frame = null;
+            f = null;
+            this.setDead();
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public int getBrightnessForRender(float dummy) {
+        return 0xF000F0;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public float getShadowSize() {
+        return 0;
+    }
+
+    @Override
+    protected void readEntityFromNBT(NBTTagCompound tag) {}
+
+    @Override
+    protected void writeEntityToNBT(NBTTagCompound tag) {}
+}
---
+++ src/main/java/alice/af/client/renderer/entity/RenderFillerFrame.java
@@ -0,0 +1,60 @@
+package alice.af.client.renderer.entity;
+
+import net.minecraft.client.renderer.RenderGlobal;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.ResourceLocation;
+
+import org.lwjgl.opengl.GL11;
+
+import alice.af.client.entity.EntityFillerFrame;
+import alice.af.tileentity.TileEntityAdvFiller;
+
+public final class RenderFillerFrame extends Render {
+
+    @Override
+    public void doRender(Entity entity, double x, double y, double z, float f1, float f2) {
+        this.render((EntityFillerFrame) entity, x, y, z);
+    }
+
+    private void render(EntityFillerFrame e, double x, double y, double z) {
+        TileEntityAdvFiller tile = e.filler.get();
+        if (tile == null) {
+            return;
+        }
+
+        double fromX = tile.startX - tile.xCoord;
+        double toX = tile.endX - tile.xCoord + 1;
+        double fromY = tile.startY - tile.yCoord;
+        double toY = tile.endY - tile.yCoord + 1;
+        double fromZ = tile.startZ - tile.zCoord;
+        double toZ = tile.endZ - tile.zCoord + 1;
+
+        tile = null;
+        AxisAlignedBB bb = AxisAlignedBB.getBoundingBox(fromX, fromY, fromZ, toX, toY, toZ);
+
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+        GL11.glPushMatrix();
+
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glDisable(GL11.GL_LIGHTING);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+
+        GL11.glTranslated(x - 0.5, y, z - 0.5);
+        GL11.glColor4d(1, 0, 0, 0.5);
+        GL11.glLineWidth(2F);
+        GL11.glDepthMask(false);
+        RenderGlobal.drawOutlinedBoundingBox(bb, -1);
+        GL11.glDepthMask(true);
+
+        GL11.glPopMatrix();
+        GL11.glPopAttrib();
+    }
+
+    @Override
+    protected ResourceLocation getEntityTexture(Entity entity) {
+        return null;
+    }
+}
